b2bd0dd1cc09514b320ff0d1fba30738a2f2db99
Khanchai <khanchai.c@ssd.co.th>
2013-08-13 16:32:51 +0700

    bowling
    bowling

---
+++ BowlingGameKata.Test/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using BowlingGameKata;
+//using Microsoft.FSharp.Collections;
+
+namespace BowlingGameScore.Tests
+{
+	[TestFixture]
+	public class CalculatorTest
+	{
+        [TestCase(0, new int[] {})]
+		[TestCase(300, new[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 })]
+		[TestCase(60, new[] { 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+		[TestCase(190, new[] { 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9 })]
+		[TestCase(110, new[] { 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1 })]
+		[TestCase(73, new[] { 5, 2, 3, 4, 4, 2, 6, 1, 8, 0, 0, 9, 2, 7, 2, 3, 8, 1, 3, 3 })]
+		public void ShouldBeCorrectSum(int actual, int[] expected)
+		{
+            var game = new BowlingGame();
+            for (int i = 0; i < expected.Length; i++ )
+            {
+                game.Turn(expected[i]);
+            }
+			Assert.AreEqual(actual, game.ScoreGame());
+
+		}
+
+        //[TestCase(0, new int[] { })]
+        //[TestCase(300, new[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 })]
+        //[TestCase(60, new[] { 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        //[TestCase(190, new[] { 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9 })]
+        //[TestCase(110, new[] { 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1 })]
+        //[TestCase(73, new[] { 5, 2, 3, 4, 4, 2, 6, 1, 8, 0, 0, 9, 2, 7, 2, 3, 8, 1, 3, 3 })]
+        //public void SumWithUnfoldShouldBeCorrect(int actual, int[] expected)
+        //{
+        //    Assert.AreEqual(actual, Calculator.CalculateScoreUnfold(expected));
+        //}
+
+	}
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class BowlingGame
+    {
+        List<int> roll = new List<int>();
+        int frameCount = 1;
+        int turnCount = 0;
+        int maxTurn = 2;
+
+        public void Turn(int pins)
+        {
+            roll.Add(pins);
+
+            if (turnCount == maxTurn)
+            {
+                if (frameCount < 10)
+                {
+                    frameCount++;
+                    turnCount = 0;
+                }
+            }
+            turnCount++;
+
+            if (pins == 10)
+            {
+                turnCount = maxTurn;
+            }
+        }
+        public int ScoreGame()
+        {
+            int turnIndex = 0;
+            int scoreFrame = 0;
+            for (int frame = 0; frame < frameCount; frame++)
+            {
+                
+                try
+                {
+                    //strike case
+                    if (roll[turnIndex] == 10)
+                    {
+                        scoreFrame += 10 + roll[turnIndex + 1] + roll[turnIndex + 2];
+                        turnIndex++;
+                    }
+                    //spare case
+                    else if (roll[turnIndex] + roll[turnIndex + 1] == 10)
+                    {
+                        scoreFrame += 10 + roll[turnIndex + 2];
+                        turnIndex += 2;
+                    }
+                    //normal case
+                    else //if (roll[turnIndex] + roll[turnIndex + 1] != 10)
+                    {
+                        scoreFrame += roll[turnIndex] + roll[turnIndex + 1];
+                        turnIndex += 2;
+                    }
+                }
+                catch { }
+            }
+            return scoreFrame;
+        }
+    }
+}
